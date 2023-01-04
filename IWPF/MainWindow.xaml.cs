6030ab27a24e3e6d510a42bee452fc4721dec787
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2023-01-04 13:34:18 +0200

    Finishing trapezoid integration

--- IWPF/MainWindow.xaml.cs
+++ IWPF/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IntegralsLib;
+using IWPF.service;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace IWPF
 {
     /// <summary>
@@ -23,6 +12,48 @@ namespace IWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void IntegrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Getting f, a, b
+            Func<double, double> f;
+            double a = 0;
+            double b = 0;
+            try
+            {
+                a = FunctionService.EvaluateAsDouble(MinTextBox.Text);
+                b = FunctionService.EvaluateAsDouble(MaxTextBox.Text);
+                f = FunctionService.CreateFunctionFromString(FunctionTextBox.Text);
+            }
+            catch 
+            {
+                ShowErrorMessageBox("Something went wrong during evaluating function, min, max. Please check input.");
+                return;
+            }
+
+            // Checking b > a
+            if (a >= b)
+            {
+                ShowErrorMessageBox("max should be BIGGER than min.");
+                return;
+            }
+
+            // Calculating epsilon
+            double epsilon = Math.Pow(10.0, -1.0 * Convert.ToDouble(EpsilonLongUpDown.Value));
+
+            // Integrating
+            IIntegrator integrator = new TrapezoidIntegrator();
+            double result = integrator.Integrate(f, a, b,epsilon);
+
+            Result.Text = "a: " + a.ToString() + " ; b: " + b.ToString() + " ; e: " + epsilon;
+            Result.Text += "\n\n";
+            Result.Text += "Result: " + result.ToString();
+        }
+
+        private void ShowErrorMessageBox(String msg)
+        {
+            MessageBox.Show(msg, "Oops!");
         }
     }
 }
---
+++ IntegralsLib/IIntegrator.cs
@@ -0,0 +1,20 @@
+﻿namespace IntegralsLib
+{
+    /// <summary>
+    /// Represents integration contract
+    /// </summary>
+    public interface IIntegrator
+    {
+        /// <summary>
+        /// Method - integration contract
+        /// </summary>
+        /// <param name="f"> function to integrate </param>
+        /// <param name="a"> min </param>
+        /// <param name="b"> max </param>
+        /// <param name="epsilon"> precision used to calculate h and n</param>
+        /// <returns> numeric integral value </returns>
+        public abstract double Integrate(Func<double, double> f, 
+                                         double a, double b,
+                                         double epsilon);
+    }
+}
